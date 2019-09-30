a2bd87fb2377eac9ca71b008e492e44ed6a401f4
Michel Hoeylaerts <michel.hoeylaerts@onespan.com>
2019-09-30 08:38:05 +0200

    test

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -1,9 +1,14 @@
+// THIS IS A TEST FOR SCC, THE KEY IS USELESS. 
+
 package com.test.HelloWorldMaven;
 
 /**
  * Hello world!
  *
  */
+ // api key for onespan cloud access.
+ private static final String API_KEY = "AKIAIOSFODNN7EXAMPLE";
+ 
 public class App 
 {
     public static void main( String[] args )
